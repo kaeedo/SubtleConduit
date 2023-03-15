44f7d47a5c5dd352eb9f9d286499c03fab844044
Kai Ito <kai.ito@valora.com>
2023-03-15 15:54:42 +0100

    Write simple tests

--- App.Test/Main.fs
+++ App.Test/Main.fs
@@ -174,6 +174,200 @@ type PlaywrightTests(outputHelper: ITestOutputHelper) =
     }
 
     [<Fact>]
+    member this.``Can sign up``() = task {
+        let launchOptions = BrowserTypeLaunchOptions()
+        launchOptions.Headless <- isHeadless
+
+        let! browser = playwright.Firefox.LaunchAsync(launchOptions)
+        let! context = browser.NewContextAsync(BrowserNewContextOptions(IgnoreHTTPSErrors = true))
+
+        let! page = context.NewPageAsync()
+
+        let! _ = page.GotoAsync("http://localhost:5173")
+
+        let signUpLink = page.GetByText("Sign up")
+
+        do! signUpLink.First.WaitForAsync()
+
+        do! signUpLink.ClickAsync()
+
+        let username = Guid.NewGuid().ToString()
+        let usernameInput = page.GetByPlaceholder("Username")
+        do! usernameInput.ClearAsync()
+        do! usernameInput.TypeAsync(username)
+
+        let email = $"{Guid.NewGuid()}@example.com"
+        let emailInput = page.GetByPlaceholder("Email")
+        do! emailInput.ClearAsync()
+        do! emailInput.TypeAsync(email)
+
+        let password = Guid.NewGuid().ToString()
+        let passwordInput = page.GetByPlaceholder("Password")
+        do! passwordInput.ClearAsync()
+        do! passwordInput.TypeAsync(password)
+
+        let signUpButton =
+            page
+                .GetByRole(AriaRole.Button)
+                .Filter(LocatorFilterOptions(HasText = "Sign up"))
+
+        do! signUpButton.ClickAsync()
+
+        let subHeader = page.GetByText("A place to share your knowledge")
+        do! subHeader.WaitForAsync()
+
+        let! isSubHeaderVisible = subHeader.IsVisibleAsync()
+
+        test <@ isSubHeaderVisible @>
+    }
+
+    [<Fact>]
+    member this.``Can publish new article``() = task {
+        let launchOptions = BrowserTypeLaunchOptions()
+        launchOptions.Headless <- isHeadless
+
+        let! browser = playwright.Firefox.LaunchAsync(launchOptions)
+        let! context = browser.NewContextAsync(BrowserNewContextOptions(IgnoreHTTPSErrors = true))
+
+        let! page = context.NewPageAsync()
+
+        let! _ = page.GotoAsync("http://localhost:5173")
+
+        let signUpLink = page.GetByText("Sign up")
+
+        do! signUpLink.First.WaitForAsync()
+
+        do! signUpLink.ClickAsync()
+
+        let username = Guid.NewGuid().ToString()
+        let usernameInput = page.GetByPlaceholder("Username")
+        do! usernameInput.ClearAsync()
+        do! usernameInput.TypeAsync(username)
+
+        let email = $"{Guid.NewGuid()}@example.com"
+        let emailInput = page.GetByPlaceholder("Email")
+        do! emailInput.ClearAsync()
+        do! emailInput.TypeAsync(email)
+
+        let password = Guid.NewGuid().ToString()
+        let passwordInput = page.GetByPlaceholder("Password")
+        do! passwordInput.ClearAsync()
+        do! passwordInput.TypeAsync(password)
+
+        let signUpButton =
+            page
+                .GetByRole(AriaRole.Button)
+                .Filter(LocatorFilterOptions(HasText = "Sign up"))
+
+        do! signUpButton.ClickAsync()
+
+        let subHeader = page.GetByText("A place to share your knowledge")
+        do! subHeader.WaitForAsync()
+
+        let newArticleLink = page.GetByText("New Article")
+        do! newArticleLink.First.WaitForAsync()
+        do! newArticleLink.ClickAsync()
+
+
+        let title = "Herp Derp Nerp Sherp"
+        let titleInput = page.GetByPlaceholder("Article Title")
+        do! titleInput.WaitForAsync()
+        do! titleInput.TypeAsync(title)
+
+        let description = "Description description description"
+        let descriptionInput = page.GetByPlaceholder("What's this article about?")
+        do! descriptionInput.WaitForAsync()
+        do! descriptionInput.TypeAsync(description)
+
+        let body =
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae purus faucibus ornare suspendisse sed. Eget magna fermentum iaculis eu. Etiam non quam lacus suspendisse faucibus interdum posuere lorem. Pellentesque massa placerat duis ultricies lacus sed turpis tincidunt. Sed enim ut sem viverra aliquet eget sit. Integer vitae justo eget magna fermentum iaculis. Porta nibh venenatis cras sed felis eget velit aliquet. Molestie a iaculis at erat pellentesque adipiscing commodo elit at. Nunc aliquet bibendum enim facilisis gravida neque. Sed felis eget velit aliquet sagittis id. Lectus quam id leo in vitae turpis massa. Quisque egestas diam in arcu cursus. Nec feugiat in fermentum posuere urna. In nisl nisi scelerisque eu ultrices vitae. Mattis molestie a iaculis at erat pellentesque adipiscing commodo elit. Elit eget gravida cum sociis natoque penatibus et magnis dis. Massa enim nec dui nunc mattis enim ut tellus elementum. Habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper."
+
+        let bodyInput = page.GetByPlaceholder("Write your article (in markdown)")
+        do! bodyInput.WaitForAsync()
+        do! bodyInput.TypeAsync(body)
+
+        let publishButton = page.GetByRole(AriaRole.Button).First
+        do! publishButton.ClickAsync()
+
+        let articleTitle = page.GetByRole(AriaRole.Heading).Nth(0)
+
+        do! articleTitle.WaitForAsync()
+        let! articleTitleText = articleTitle.TextContentAsync()
+
+        test <@ articleTitleText = title @>
+    }
+
+    [<Fact>]
+    member this.``Can favorite an article``() = task {
+        let launchOptions = BrowserTypeLaunchOptions()
+        launchOptions.Headless <- isHeadless
+
+        let! browser = playwright.Firefox.LaunchAsync(launchOptions)
+        let! context = browser.NewContextAsync(BrowserNewContextOptions(IgnoreHTTPSErrors = true))
+
+        let! page = context.NewPageAsync()
+
+        let! _ = page.GotoAsync("http://localhost:5173")
+
+        let signUpLink = page.GetByText("Sign up")
+
+        do! signUpLink.First.WaitForAsync()
+
+        do! signUpLink.ClickAsync()
+
+        let username = Guid.NewGuid().ToString()
+        let usernameInput = page.GetByPlaceholder("Username")
+        do! usernameInput.ClearAsync()
+        do! usernameInput.TypeAsync(username)
+
+        let email = $"{Guid.NewGuid()}@example.com"
+        let emailInput = page.GetByPlaceholder("Email")
+        do! emailInput.ClearAsync()
+        do! emailInput.TypeAsync(email)
+
+        let password = Guid.NewGuid().ToString()
+        let passwordInput = page.GetByPlaceholder("Password")
+        do! passwordInput.ClearAsync()
+        do! passwordInput.TypeAsync(password)
+
+        let signUpButton =
+            page
+                .GetByRole(AriaRole.Button)
+                .Filter(LocatorFilterOptions(HasText = "Sign up"))
+
+        do! signUpButton.ClickAsync()
+
+        let subHeader = page.GetByText("A place to share your knowledge")
+        do! subHeader.WaitForAsync()
+
+        let favoriteButton = page.GetByRole(AriaRole.Button).First
+        do! favoriteButton.ClickAsync()
+
+        let profileLink = page.GetByText(username).First
+        do! profileLink.ClickAsync()
+
+        let bannerName = page.GetByRole(AriaRole.Heading).Nth(1)
+        do! bannerName.WaitForAsync()
+
+        let! name = bannerName.TextContentAsync()
+        test <@ name = username @>
+
+        let favoritePostsTab = page.GetByText("Favorited Posts")
+        do! favoritePostsTab.ClickAsync()
+
+        let favoriteButtonInFeedItem =
+            page
+                .GetByTestId("feedItems")
+                .First.GetByRole(AriaRole.Button)
+
+        do! favoriteButtonInFeedItem.First.WaitForAsync()
+
+        let! favoritedItemsCount = favoriteButtonInFeedItem.CountAsync()
+
+        test <@ favoritedItemsCount = 1 @>
+    }
+
+    [<Fact>]
     member this.``Run Scrutiny Test``() = task {
         let launchOptions = BrowserTypeLaunchOptions()
         launchOptions.Headless <- isHeadless
